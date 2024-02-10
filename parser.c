8811b4db3aea011fa6fce93a4cefdfa8c7bf73ca
Joshua <98911854+j000shua@users.noreply.github.com>
2024-02-10 18:27:21 +0100

    Add files via upload

--- parser.c
+++ parser.c
@@ -6,12 +6,12 @@
 /*   By: jlinguet <jlinguet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 14:49:18 by jlinguet          #+#    #+#             */
-/*   Updated: 2024/02/10 16:57:41 by jlinguet         ###   ########.fr       */
+/*   Updated: 2024/02/10 17:38:27 by jlinguet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
+/*
 static char	**ft_append(char **tab, char *s)
 {
 	int		i;
@@ -33,7 +33,7 @@ static char	**ft_append(char **tab, char *s)
 	new_tab[i + 1] = NULL;
 	return (free(tab), new_tab);
 }
-
+*/
 t_cmd	parse(int ac, char **av)
 {
 	t_cmd	cmd;
@@ -43,10 +43,11 @@ t_cmd	parse(int ac, char **av)
 		return (perror("Incorrect arguments"), cmd);
 	if (access(av[1], R_OK))
 		return (perror("Cannot accoss or read file1"), cmd);
+	cmd.file1 = av[1];
 	cmd.prog1 = ft_split(av[2], ' ');
 	if (access(cmd.prog1[0], F_OK))
 		return (perror("Cannot access cmd1"), cmd);
-	cmd.prog1 = ft_append(cmd.prog1, av[1]);
+	//cmd.prog1 = ft_append(cmd.prog1, av[1]);
 	cmd.prog2 = ft_split(av[3], ' ');
 	if (access(cmd.prog2[0], F_OK))
 		return (perror("Cannot access cmd2"), cmd);
--- pipex.c
+++ pipex.c
@@ -6,18 +6,18 @@
 /*   By: jlinguet <jlinguet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 13:39:36 by jlinguet          #+#    #+#             */
-/*   Updated: 2024/02/10 16:57:56 by jlinguet         ###   ########.fr       */
+/*   Updated: 2024/02/10 18:05:41 by jlinguet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int ac, char **av)
+int	main(int ac, char **av, char **env)
 {
 	int		fd[2];
 	pid_t	pid;
 	t_cmd	cmd;
-	int		file2;
+	int		file;
 
 	cmd = parse(ac, av);
 	if (!cmd.file2)
@@ -27,24 +27,25 @@ int	main(int ac, char **av)
 	pid = fork();
 	if (pid == CHILD_PROCESS)
 	{
+		file = open(cmd.file1, O_RDONLY);
 		//char *arg[] = { "/bin/ls", "-l", ".", NULL};
 		dup2(fd[WR_END], STDOUT_FILENO);
 		close(fd[WR_END]);
 		close(fd[RD_END]);
-		execve(cmd.prog1[0], cmd.prog1, NULL);
-		return (0);
+		if(execve(cmd.prog1[0], cmd.prog1, env))
+			return (perror("Execution of cmd1 failed"), 1);
 	}
 	else if (pid < 0)
 		return (perror("Fork failed"), 1);
 	else
 	{
-		file2 = open(cmd.file2, O_RDWR | O_CREAT, BASIC_FILE_MODE);
+		file = open(cmd.file2, O_WRONLY | O_CREAT, BASIC_FILE_MODE);
 		//char *arg[] = {"/usr/bin/wc", "-l", NULL};
-		dup2(file2 STDOUT_FILENO);
+		dup2(file, STDOUT_FILENO);
 		dup2(fd[RD_END], STDIN_FILENO);
 		close(fd[WR_END]);
 		close(fd[RD_END]);
-		execve(cmd.prog2[0], cmd.prog2, NULL);
-		return (0);
+		if(execve(cmd.prog2[0], cmd.prog2, env))
+			return (perror("Execution of cmd2 failed"), 1);
 	}
 }
